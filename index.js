2eacc5bf52210b0d53662e4e0dad00991f10ae10
erick oliva <eoliva@trigarante.com>
2022-11-09 20:28:32 -0600

    prueba get

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.listen(PUERTO, function(){
 //     console.log('Se recibio una petición get');
 // });
 
-// app.get('/', db.getUsers);
+app.get('/', db.getUsers);
 
-// app.get('/persona', db.getUsers);
-// app.get('/persona', db.getUsers);
-app.use('/', function(req, res){
-    res.send('Hola, estas en la pagina inicial');
-    console.log('Se recibio una petición get 2');
-});
-app.use('/persona', db.getUsers);
+app.get('/persona', db.getUsers);
+// // app.get('/persona', db.getUsers);
+// app.use('/', function(req, res){
+//     res.send('Hola, estas en la pagina inicial');
+//     console.log('Se recibio una petición get 2');
+// });
+// app.use('/persona', db.getUsers);
