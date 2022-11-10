71b867a1f0f030dd825dec5ddbd6637b9dc0856f
erick oliva <eoliva@trigarante.com>
2022-11-10 17:12:28 -0600

    router

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
 const db = require('./query')
 
 const PUERTO = 3002;
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+app.get('/users', db.getUsers);
 
 app.listen(PUERTO, function(){
     console.log('Servidor http correindo en el puerto 3002');
@@ -17,9 +29,9 @@ app.listen(PUERTO, function(){
 //     console.log('Se recibio una petición get');
 // });
 
-app.get('/', db.getUsers);
-
-app.get('/persona', db.getUsers);
+// app.get('/', db.getUsers);
+//
+// app.get('/persona', db.getUsers);
 // // app.get('/persona', db.getUsers);
 // app.use('/', function(req, res){
 //     res.send('Hola, estas en la pagina inicial');
