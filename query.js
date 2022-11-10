e798998f88817eaa9e07c8be24c412ad945ecf41
erick oliva <eoliva@trigarante.com>
2022-11-09 20:43:37 -0600

    cors

--- query.js
+++ query.js
@@ -11,6 +11,11 @@ const getUsers = (request, response) => {
         if (error) {
             throw error
         }
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        response.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
         response.status(200).json(results.rows)
     })
 }
