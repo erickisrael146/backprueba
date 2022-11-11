0053fd6018b5c88c43ac3d77df69c064dee30fdc
erick oliva <eoliva@trigarante.com>
2022-11-10 19:56:44 -0600

    error

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const db2 = require('./query');
 
 /* GET users listing. */
-router.get('/',db.getUsers);
+router.get('/', db2.getUsers);
 
 // router.get('/nuevo', (request, response) => {
 //     response.json({ info: 'Node.js, Express, and Postgres API' })
